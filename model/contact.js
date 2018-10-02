44acec0993f5538159cc7c8b0ea671fc0dc0c673
Shakspeare A <shakspeare.a@gmail.com>
2018-10-02 20:19:46 +0530

    clean

--- model/contact.js
+++
@@ -1,9 +0,0 @@
-var mongoose=require('mongoose')
-module.exports = mongoose.model('Contact',{
-    domain: String,
-    name:String,
-    phone:String,
-    email:String,
-    subject:String,
-    message:String
-});
